505dfb5661f5724a5eb3bb81fce65ce42363f45c
bladyjoker <bladyjoker@bladyjoker-glaptop0.roam.corp.google.com>
2017-06-12 18:33:24 +0200

    Aestethics.

--- regen/src/Control/Monad/Logic/RE2.hs
+++ regen/src/Control/Monad/Logic/RE2.hs
@@ -1,8 +1,8 @@
 module Control.Monad.Logic.RE2 where
 
-import Control.Monad.Logic
-import Control.Monad
 import Control.Applicative
+import Control.Monad
+import Control.Monad.Logic (runLogic, (>>-), interleave, Logic)
 
 -- Enumerate sentences given a grammar G
 
@@ -27,37 +27,37 @@ orL = interleave
 andL :: Logic Sentence -> Logic Sentence -> Logic Sentence
 andL lx ly = lx >>- (\x -> (ly >>- (\y -> return $ x ++ y)))
 
-runRE2 expr = runLogic (app expr) (:) []
+runRE2 expr = runLogic (logic expr) (:) []
 
-app :: Expr -> Logic Sentence
-app (And l r) = app l `andL` app r
-app (Or l r) = app l `orL` app r
-app (Literal c) = return [c]
-app Empty = return ""
+logic :: Expr -> Logic Sentence
+logic (And l r) = logic l `andL` logic r
+logic (Or l r) = logic l `orL` logic r
+logic (Literal c) = return [c]
+logic Empty = return ""
 
-app (OneOrMore expr) = app $ Or expr (And expr (OneOrMore expr)) -- <expr>+ == <expr>|<expr><expr>+
+logic (OneOrMore expr) = logic $ Or expr (And expr (OneOrMore expr)) -- <expr>+ == <expr>|<expr><expr>+
 
-app (Group expr) = app expr -- (<expr>) == <expr>
+logic (Group expr) = logic expr -- (<expr>) == <expr>
 
-app (ZeroOrOne expr) = app $ Or Empty expr -- <expr>? == <empty>|<expr>
+logic (ZeroOrOne expr) = logic $ Or Empty expr -- <expr>? == <empty>|<expr>
 
-app (ZeroOrMore expr) =  app $
+logic (ZeroOrMore expr) =  logic $
   Or (ZeroOrOne expr) (And expr (OneOrMore expr)) -- <expr>* == <empty>?|<expr><expr>+
 
-app (Exactly n expr) =  app $
+logic (Exactly n expr) =  logic $
   if n > 0
   then And expr (Exactly (n-1) expr)
   else Empty -- <expr>{n},n>0 == <expr><expr>{n-1} <expr>{0} == <empty>
 
-app (FromNtoM n m expr) =  app $
+logic (FromNtoM n m expr) =  logic $
   if n < m
   then Or (Exactly n expr) (FromNtoM (n+1) m expr)
   else Exactly m expr -- <expr>{n,m},n<m == <expr>{n}|<expr>{n+1,m} <expr>{m,m} == <expr>{m}
 
-app (NorMore n expr) = app $
+logic (NorMore n expr) = logic $
   Or (Exactly n expr) (NorMore (n+1) expr) -- <expr>{n,} == <expr>{n} | <expr>{n+1,}
 
-app (NorLess n expr) = app $
+logic (NorLess n expr) = logic $
   if n > 0
   then Or (Exactly n expr) (NorLess (n-1) expr)
   else Empty
